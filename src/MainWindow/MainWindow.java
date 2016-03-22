b6b2679e004f73576bf15025346a2ef606e65c05
saiohao <442406628@qq.com>
2016-03-22 16:44:21 +0800

    3.22 16:44

--- src/MainWindow/MainWindow.java
+++ src/MainWindow/MainWindow.java
@@ -1,41 +1,42 @@
 package MainWindow;
-import java.awt.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;  
-  
-class Windows extends JFrame   
-{  
-    MenuBar menubar;  
-    Menu menu;  
-    MenuItem item1, item2;  
-    Windows(String s)  
-    {  
-        setTitle(s);
-        Toolkit tool = getToolkit();  
-        Dimension dim = tool.getScreenSize();  
-        setBounds(0, 0, dim.width/2, dim.height/2);  
-        menubar = new MenuBar();  
-        menu = new Menu("文件");  
-        item1 = new MenuItem("打开");  
-        item2 = new MenuItem("保存");  
-        menu.add(item1);  
-        menu.add(item2);  
-        menubar.add(menu);  
-        setMenuBar(menubar); 
-        JButton videoButton = new JButton("开启摄像头");
-        this.add(videoButton);
-        videoButton.setBounds(0, 0, 10, 10);
-        
-        
-        setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }  
-}  
-public class MainWindow {  
-    public static void main(String args[])  
-    {  
-        Windows win = new Windows("RealTimeDetector");  
-    }  
-  
-}  
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow frame = new MainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
