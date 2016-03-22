fb95409e80097856f851f5035f23861362c7a9d8
saiohao <442406628@qq.com>
2016-03-22 16:17:38 +0800

    zhouhao

--- src/OpencvTest.java
+++ src/OpencvTest.java
@@ -6,9 +6,7 @@ public class OpencvTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.loadLibrary("opencv_java2411");  
-        Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);  
-        System.out.println("m = " + m.dump()); 
+
 	}
 
 }
